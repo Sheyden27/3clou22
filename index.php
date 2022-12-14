a0d24ea5ea0cb4bc55720357184c11291495a7e7
mounir.zaouiti <72012187+Sheyden27@users.noreply.github.com>
2022-12-14 10:04:57 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head><title>Mon appli PHP</title>
+
+<body>
+
+<h1>Une application déployée sur le Cloud</h1>
+
+<p>
+<?php
+
+  echo "Nom du serveur ", gethostname();
+  echo "<br/>";
+  echo "Nous sommes le ", date("d M Y h:i:s");
+
+?>
+</p>
+
+</body>
